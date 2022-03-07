1de682559711457ca5772eb796ca31bfff234b93
fabiochidda <fabio.chidda99@gmail.com>
2022-03-07 03:41:34 +0100

    Aggiunto Axios e creato un ciclo per generare le email

--- app.js
+++ app.js
@@ -3,9 +3,22 @@ const app = new Vue ({
     el: "#app",
     data: {
 
+        emailArray: [],
 
+    },
+    created() {
 
+        for(let i=0; i<10; i++) {
 
-    }
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then((res) => {
+    
+                this.emailArray.push(res.data.response)
+    
+            })
+
+        }
+
+    },
 
 })
